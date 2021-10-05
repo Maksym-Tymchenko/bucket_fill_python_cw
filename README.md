ae28d308721c86524da7317f715ebb99e92ca8d7
Luca Grillotti <luca.grillotti16@imperial.ac.uk>
2021-10-05 19:25:23 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# CO70053 - Python Programming - CW1 - Bucket Fill
+
+_Task:_ Complete the [bucket_fill.py](bucket_fill/bucket_fill.py) file.
+
+You are encouraged to read the **DocStrings** present in the document.
+
+To test your code you can use the following command:
+```shell
+python bucket_fill/bucket_fill.py
+```
+
+The loaded pattern file is available in [data/](data/).
---
+++ bucket_fill/bucket_fill.py
@@ -0,0 +1,113 @@
+def load_image(filename):
+    """ Load image from file made of 0 (unfilled pixels), 1 (boundary pixels) and 2 (filled pixel)
+
+    Example of content of filename:
+
+0 0 0 0 1 1 0 0 0 0
+0 0 1 1 0 0 1 1 0 0
+0 1 1 0 0 1 0 1 1 0
+1 1 0 0 1 0 1 0 1 1
+1 0 0 1 0 0 1 0 0 1
+1 0 0 1 0 0 1 0 0 1
+1 1 0 1 0 0 1 0 1 1
+0 1 1 0 1 1 0 1 1 0
+0 0 1 1 0 0 1 1 0 0
+0 0 0 0 1 1 0 0 0 0
+
+    Args:
+        filename (str) : path to file containing the image representation
+
+    Returns:
+        list : a 2D representation of the filled image, where
+               0 represents an unfilled pixel,
+               1 represents a boundary pixel
+               2 represents a filled pixel
+    """
+
+    image = []
+    with open(filename) as imagefile:
+        for line in imagefile:
+            if line.strip():
+                row = list(map(int, line.strip().split()))
+                image.append(row)
+    return image
+
+
+def get_str_image(image):
+    mapping = {
+        0: " ",
+        1: "*",
+        2: "0"
+    }
+
+    image_str = ""
+    if image:
+        image_str += "_ " * (len(image[0]) + 2) + "\n"
+    for row in image:
+        image_str += "| "
+        for pixel in row:
+            image_str += mapping.get(pixel, "?") + " "
+        image_str += "|"
+        image_str += '\n'
+    if image:
+        image_str += "‾ " * (len(image[0]) + 2) + "\n"
+
+    return image_str
+
+
+def show_image(image):
+    """ Show image in terminal
+
+    Args:
+        image (list) : list of lists of 0 (unfilled pixel), 1 (boundary pixel) and 2 (filled pixel)
+
+    Note:
+        The variable "mapping" defines how to display each type of pixel.
+
+    Returns:
+        None
+    """
+
+    print(get_str_image(image))
+
+
+def fill(image, seed_point):
+    """ Fill the image from seed point to boundary
+
+    the image should remain unchanged if:
+    - the seed_point has a non-integer coordinate
+    - the seed_point is on a boundary pixel
+    - the seed_point is outside of the image
+
+    Args:
+        image (list) : a 2D nested list representation of an image, where
+                       0 represents an unfilled pixel, and
+                       1 represents a boundary pixel
+        seed_point (tuple) : a 2-element tuple representing the (row, col) of
+                       the seed point to start filling
+
+    Returns:
+        list : a 2D representation of the filled image, where
+               0 represents an unfilled pixel,
+               1 represents a boundary pixel, and
+               2 represents a filled pixel
+    """
+
+    # TODO: to complete
+
+
+def test_bucket_fill():
+    image = load_image("data/test_pattern.txt")
+
+    print("Before filling:")
+    show_image(image)
+
+    image = fill(image=image, seed_point=(7, 3))
+
+    print("-" * 25)
+    print("After filling:")
+    show_image(image)
+
+
+if __name__ == '__main__':
+    test_bucket_fill()

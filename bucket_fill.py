57e6216c0ff01040d416b78736d1c3302f8cbdec
Maksym Tymchenko <maksymtymchenko@pc-139-56.customer.ask4.lan>
2021-10-15 13:43:25 +0100

    Addressed pycodestyle feedback

--- bucket_fill.py
+++ bucket_fill.py
@@ -1,8 +1,9 @@
 """ Coursework 1: Bucket Fill
 """
 
+
 def load_image(filename):
-    """ Load image from file made of 0 (unfilled pixels) and 1 (boundary pixels) and 2 (filled pixel)
+    """Load image from file made of 0 (unfilled pixels) and 1 (boundary pixels) and 2 (filled pixel)
 
     Example of content of filename:
 
@@ -26,7 +27,6 @@ def load_image(filename):
                1 represents a boundary pixel
                2 represents a filled pixel
     """
-
     image = []
     with open(filename) as imagefile:
         for line in imagefile:
@@ -37,7 +37,7 @@ def load_image(filename):
 
 
 def stringify_image(image):
-    """ Convert image representation into a human-friendly string representation
+    """Convert image representation into a human-friendly string representation
 
     Args:
         image (list) : list of lists of 0 (unfilled pixel), 1 (boundary pixel) and 2 (filled pixel)
@@ -45,7 +45,6 @@ def stringify_image(image):
     Returns:
         str : a human-friendly string representation of the image
     """
-    
     if image is None:
         return ""
 
@@ -72,7 +71,7 @@ def stringify_image(image):
 
 
 def show_image(image):
-    """ Show image in terminal
+    """Show image in terminal.
 
     Args:
         image (list) : list of lists of 0 (unfilled pixel), 1 (boundary pixel) and 2 (filled pixel)
@@ -81,7 +80,7 @@ def show_image(image):
 
 
 def fill(image, seed_point):
-    """ Fill the image from seed point to boundary
+    """Fill the image from seed point to boundary.
 
     the image should remain unchanged if:
     - the seed_point has a non-integer coordinate
@@ -92,7 +91,7 @@ def fill(image, seed_point):
         image (list) : a 2D nested list representation of an image, where
                        0 represents an unfilled pixel, and
                        1 represents a boundary pixel
-        seed_point (tuple) : a 2-element tuple representing the (row, col) 
+        seed_point (tuple) : a 2-element tuple representing the (row, col)
                        coordinates of the seed point to start filling
 
     Returns:
@@ -101,20 +100,19 @@ def fill(image, seed_point):
                1 represents a boundary pixel, and
                2 represents a filled pixel
     """
-
     # TODO: Complete this function
     # Extract row and col coordinates of the seed
-    row,col = seed_point
+    (row, col) = seed_point
 
     # Calculate num rows and cols of the image
     num_rows = len(image)
     num_cols = len(image[0])
-    
+
     # Return original image if seed has non integer coordinates
-    if not (isinstance(row,int) and  isinstance(col,int)):
+    if not (isinstance(row, int) and isinstance(col, int)):
         # print("Seed does not contain integer coordinates. Returning original image.")
         return image
-    
+
     # Return original image if seed has coordinates outside of the image
     is_inside_row = (0 <= row < num_rows)
     is_inside_col = (0 <= col < num_cols)
@@ -129,16 +127,16 @@ def fill(image, seed_point):
         image[row][col] = 2
 
         # Apply fill function to all four adjacent cells
-        image = fill(image,(row-1,col))
-        image = fill(image,(row,col+1))
-        image = fill(image,(row+1,col))
-        image = fill(image,(row,col-1)) 
-    
+        image = fill(image, (row-1, col))
+        image = fill(image, (row, col+1))
+        image = fill(image, (row+1, col))
+        image = fill(image, (row, col-1))
+
     return image
 
 
 def create_square_image(n=25):
-    """ Write an image of zeros and an image of ones to separate files
+    """Write an image of zeros and an image of ones to separate files.
 
     Args:
         n (int) : size of the desired image
@@ -146,13 +144,12 @@ def create_square_image(n=25):
     Returns:
         tuple : (path_to_unfilled_image, path_to_filled_image) where each entry is a string
     """
-
     # Define path to unfilled image
     file_path_unfilled = "./data/" + "square_image_" + str(n) + ".txt"
 
     # Define path to expected filled image
     file_path_filled = "./data/" + "expected_" + "square_image_" + str(n) + ".txt"
-    
+
     # Write image to file
     with open(file_path_unfilled, 'w') as f:
         line = n*"0 "+"\n"
@@ -182,40 +179,26 @@ def example_fill():
     show_image(image)
 
 
-def example_fill_custom():
-    image = load_image("data/smiley.txt")
-
-    print("Before filling:")
-    show_image(image)
-    image = fill(image=image, seed_point=(7,7))
-
-    print("-" * 25)
-    print("After filling:")
-    show_image(image)
- 
-
 def test_invalid_seed():
-    """ Check that the fill function returns the original image when given invalid seeds."""
+    """Check that the fill function returns the original image when given invalid seeds."""
     # Create a list of invalid seeds
-    seed_list = [(1.5,2),(-2,0),("myseed",3)]
+    seed_list = [(1.5, 2), (-2, 0), ("myseed", 3)]
     image_input = load_image("data/bar.txt")
-    
+
     # Check that the input image is returned with invalid seeds
     for seed in seed_list:
         image_output = fill(image=image_input, seed_point=seed)
-        assert(image_input == image_output)
+        assert (image_input == image_output), f"Input seed {seed} should return the original image."
 
- 
+
 def test_image(image_path, image_expected_path, seed_point):
-    """ Check that the fill function returns the expected image.
+    """Check that the fill function returns the expected image.
 
     Args:
         image_path (str) : path to file containing the image representation
         image_expected_path (str) : path to file containing the expected image representation
         seed_point (tuple) : a 2-element tuple representing the (row, col)
                        coordinates of the seed point to start filling
-    Returns:
-        boolean : True if test passed, False otherwise
 
     """
     image = load_image(image_path)
@@ -233,24 +216,17 @@ def test_image(image_path, image_expected_path, seed_point):
     print("Expected image:")
     show_image(image_expected)
 
-    if image_filled == image_expected:
-        print("Your image looks as expected!\n")
-        return True
-    else:
-        print("Your image does not look as expected!\n")
-        return False
+    assert (image_filled == image_expected), "Your image does not look as expected!\n"
+    print("Your image looks as expected!\n")
 
 
 def test_large_image():
-    """ Check that the fill function returns the original image when given a large 25x25 image input.
-       
-        Returns:
-            boolean : True if test passed, False otherwise
-
+    """Check that the fill function returns the original image
+    when given a large 25x25 image input.
     """
     # Create empty square image of size n
     n = 25
-    image_file,expected_image_file = create_square_image(n)
+    (image_file, expected_image_file) = create_square_image(n)
     image = load_image(image_file)
     image_expected = load_image(expected_image_file)
 
@@ -264,23 +240,24 @@ def test_large_image():
     show_image(image_filled)
 
     print("Expected image:")
-    show_image(image_expected)    
+    show_image(image_expected)
 
-    if image_filled == image_expected:
-        print("Your image looks as expected!\n")
-        return True
-    else:
-        print("Your image does not look as expected!\n")
-        return False      
-
+    assert (image_filled == image_expected), "Your image does not look as expected!\n"
+    print("Your image looks as expected!\n")
 
 
 if __name__ == '__main__':
     # Test invalid seeds
     test_invalid_seed()
+
+    # Test that a seed on the boundary returns the original image
+    test_image("data/bar.txt", "data/bar.txt", (3, 7))
+
     # Test bar.txt image
-    test_image("data/bar.txt","data/expected_bar.txt",(7,7))
+    test_image("data/bar.txt", "data/expected_bar.txt", (7, 7))
+
     # Test smiley.txt image
-    test_image("data/smiley.txt","data/expected_smiley.txt",(7,7))
+    test_image("data/smiley.txt", "data/expected_smiley.txt", (7, 7))
+
     # Test large image
     test_large_image()
